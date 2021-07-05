878d57448daee4ca021a2a22d1b5ba1888459cb4
John Zhao <joh.zhao@gmail.com>
2021-07-05 12:01:36 +0800

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
-import other
+
+from .other import init_in_other, run_in_other, teardown_in_other
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
@@ -7,14 +8,14 @@ logger.addHandler(logging.NullHandler())
 
 def init():
     logger.info('init')
-    other.init_in_other()
+    init_in_other()
 
 
 def run():
     logger.info('run')
-    other.run_in_other()
+    run_in_other()
 
 
 def teardown():
     logger.info('teardown')
-    other.teardown_in_other()
+    teardown_in_other()
