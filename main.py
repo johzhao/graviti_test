425fca40978ee19b1ce34c2885649412837567a4
John Zhao <joh.zhao@gmail.com>
2021-07-05 17:28:20 +0800

    update

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import logging
 import sys
 
+import other
+
 logger = logging.getLogger(__name__)
-logger.addHandler(logging.NullHandler())
 
 
 def main():
     logger.info(f'execute task with args {sys.argv}')
+    other.some_func()
 
 
 if __name__ == '__main__':
-    main()
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s (%(name)s) [%(levelname)s]: %(message)s')
+    try:
+        main()
+    except Exception as e:
+        logger.exception(f'exception:\n{e}')
+    finally:
+        logger.info('all finished.')
---
+++ other.py
@@ -0,0 +1,7 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def some_func():
+    logger.info('execute some_func in other')
