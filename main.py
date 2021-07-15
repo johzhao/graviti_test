d86e00d1ec473e80b47adf61a77a2cdb678bb0af
John Zhao <joh.zhao@gmail.com>
2021-07-15 08:52:20 +0800

    update with filter func

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Callable
 
 from .other import init_in_other, run_in_other, teardown_in_other
 
@@ -6,13 +7,18 @@ logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
 
-def init():
+def user_filter():
+    pass
+
+
+def init(params: dict) -> Callable:
     logger.info('init')
     init_in_other()
+    return user_filter
 
 
-def run(data: dict, params: dict = None) -> bool:
-    logger.info(f'run with data {data} and params {params}')
+def run(data: dict, filter_fun: Callable = None) -> bool:
+    logger.info(f'run with data {data}')
     run_in_other()
     return True
 
